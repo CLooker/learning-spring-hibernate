e267e37039b9051d22f813f88fc84ea6fffd3e6f
Chad Looker <chad.looker@gmail.com>
2020-05-05 18:38:07 -0400

    Start hibernate

---
+++ projects/hibernate/src/main/java/com/clooker/learning_spring_hibernate/App.java
@@ -0,0 +1,13 @@
+package com.clooker.learning_spring_hibernate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
