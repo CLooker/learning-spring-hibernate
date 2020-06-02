043c05f1866c4719e9d5be009b9010d0f952ea16
Chad Looker <chad.looker@gmail.com>
2020-06-02 13:00:43 -0400

    Add basic maven project archetype example

---
+++ projects/coolapp/src/main/java/com/clooker/learning_spring_hibernate/App.java
@@ -0,0 +1,7 @@
+package com.clooker.learning_spring_hibernate;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World of Maven!");
+  }
+}
---
+++ projects/coolapp/src/test/java/com/clooker/learning_spring_hibernate/AppTest.java
@@ -0,0 +1,18 @@
+package com.clooker.learning_spring_hibernate;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+  /**
+   * Rigorous Test :-)
+   */
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
