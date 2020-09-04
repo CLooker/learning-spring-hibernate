2de117ce4746bcb49cdb99a14c70ca87e717b5c8
Chad Looker <chad.looker@gmail.com>
2020-09-04 08:16:54 -0400

    Add demos for spring-boot-actuator, spring-boot-cli, spring-boot-properties

--- projects/spring-boot-properties-demo/src/main/java/com/clooker/springbootdemo/rest/FunRestController.java
+++ projects/spring-boot-properties-demo/src/main/java/com/clooker/springbootdemo/rest/FunRestController.java
@@ -1,10 +1,17 @@
 package com.clooker.springbootdemo.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public final class FunRestController {
+
+  @Value("${coach.name}")
+  private String coachName;
+
+  @Value("${team.name}")
+  private String teamName;
 
   @GetMapping("/")
   public String helloWorld() {
@@ -19,6 +26,11 @@ public final class FunRestController {
   @GetMapping("/fortune")
   public String fortune() {
     return "You will be taken to a far off land and find meaning there...";
+  }
+
+  @GetMapping("/teaminfo")
+  public String teamInfo() {
+    return "Coach: " + coachName + ", " + "Team: " + teamName;
   }
 
 }
